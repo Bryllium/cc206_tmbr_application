259c5035a8819cb736fb9c17e1aa65570442d8c2
Bryllium <124789797+Bryllium@users.noreply.github.com>
2023-12-06 10:14:58 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// main.dart
 import 'package:flutter/material.dart';
 import 'homepage.dart';
 
@@ -18,73 +17,37 @@ class MyApp extends StatelessWidget {
             ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 75, 97, 196)),
         useMaterial3: true,
       ),
-      home: const LandingPage(),
+      home: MainPage(),
     );
   }
 }
 
-class LandingPage extends StatelessWidget {
-  const LandingPage({Key? key}) : super(key: key);
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Logo placeholder
-            Container(
-              width: 100, // Set the desired width for the logo
-              height: 100, // Set the desired height for the logo
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                  image: AssetImage(
-                      'assets/images/tmbr(Logo).png'), // Replace 'assets/logo.png' with your actual logo asset path
-                  fit: BoxFit.cover,
-                ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-            const SizedBox(height: 20), // Add some space here
-
             const Text(
               'Welcome to TMBR!',
               style: TextStyle(
-                fontSize: 50,
+                fontSize: 40,
                 fontWeight: FontWeight.bold,
-                fontFamily: 'Chonburi', // Use the specified font family
               ),
             ),
-            const SizedBox(height: 40), // Add some space here
-            Container(
-              width: 200, // Set the desired width for the button
-              height: 60, // Set the desired height for the button
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 75, 97, 196),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            const MyHomePage(title: 'Dashboard')),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  shadowColor: Colors.transparent,
-                ),
-                child: const Text(
-                  'Go to Dashboard',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-              ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LandingPage()),
+                );
+              },
+              child: const Text('Go to Homepage'),
             ),
           ],
         ),
