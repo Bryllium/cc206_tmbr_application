c86445cee94b948ad05e6ccb11bac41a1064c2ac
Bryllium <124789797+Bryllium@users.noreply.github.com>
2023-12-06 08:35:44 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,93 @@
+// main.dart
 import 'package:flutter/material.dart';
-import 'package:cc206_tmbr_application/features/copy_and_paste_page.dart';
-import 'package:cc206_tmbr_application/features/text_to_speech_page.dart';
-import 'package:cc206_tmbr_application/components/home_drawer.dart';
-import 'package:cc206_tmbr_application/components/home_tabs.dart';
+import 'homepage.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: MyHomePage(),
-    theme: ThemeData(
-      fontFamily: 'Architects_Daughter',
-    ),
-    routes: {
-      '/copyAndPaste': (context) => CopyAndPastePage(),
-      '/textToSpeech': (context) => TextToSpeechPage(),
-    },
-  ));
+  runApp(const MyApp());
 }
 
-class MyHomePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo Home Page'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 75, 97, 196)),
+        useMaterial3: true,
+      ),
+      home: const LandingPage(),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Logo placeholder
+            Container(
+              width: 100, // Set the desired width for the logo
+              height: 100, // Set the desired height for the logo
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: AssetImage(
+                      'assets/images/tmbr(Logo).png'), // Replace 'assets/logo.png' with your actual logo asset path
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            const SizedBox(height: 20), // Add some space here
+
+            const Text(
+              'Welcome to TMBR!',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Chonburi', // Use the specified font family
+              ),
+            ),
+            const SizedBox(height: 40), // Add some space here
+            Container(
+              width: 200, // Set the desired width for the button
+              height: 60, // Set the desired height for the button
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 75, 97, 196),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const MyHomePage(title: 'Dashboard')),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                ),
+                child: const Text(
+                  'Go to Dashboard',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            ),
+          ],
         ),
-        drawer: HomeDrawer(),
-        body: HomeTabs(),
       ),
     );
   }
